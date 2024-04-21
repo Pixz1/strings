2d6561a47b3a39f80fa8d6fb13a28050fd82c101
Pixz1 <115209263+Pixz1@users.noreply.github.com>
2024-04-21 15:30:27 +1000

    add fetchUser function to accountprofile component

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,6 +1,8 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 async function Page() {
     const user = await currentUser();
@@ -8,15 +10,16 @@ async function Page() {
     // to avoid typescript warnings
     if (!user) return null;
 
-    const userInfo = {};
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
 
     const userData = {
         id: user?.id,
         objectId: userInfo?._id,
-        username: userInfo?.username || user?.username,
-        name: userInfo?.name || user?.firstName || "",
-        bio: userInfo?.bio || "",
-        image: userInfo?.image || user?.imageUrl,
+        username: userInfo ? userInfo?.username : user.username,
+        name: userInfo ? userInfo?.name : user?.firstName ?? "",
+        bio: userInfo ? userInfo?.bio : "",
+        image: userInfo ? userInfo?.image : user?.imageUrl,
     };
 
     return (
