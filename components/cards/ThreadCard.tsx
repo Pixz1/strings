31191bced26c4f9fbbf8213ee261a570a3df5f7a
Pixz1 <115209263+Pixz1@users.noreply.github.com>
2024-04-21 20:53:29 +1000

    Update style of thread card based on whether or not the thread is a comment

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -76,7 +76,11 @@ const ThreadCard = ({
                         </p>
 
                         {/* links and replies display */}
-                        <div className="mt-5 flex flex-col gap-3">
+                        <div
+                            className={`${
+                                isComment && "mb-10"
+                            } mt-5 flex flex-col gap-3`}
+                        >
                             <div className="flex gap-3.5">
                                 <Image
                                     src="/assets/heart-gray.svg"
